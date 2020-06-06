7f876149621cc7c4b3d137767b6abd2c862ec445
OSCARIVAN2RAMOSGARCIA <62730004+OSCARIVAN2RAMOSGARCIA@users.noreply.github.com>
2020-06-06 15:33:49 -0500

    Update symbols.js

--- code/repositorio-ds/3p/ejemplos/gro-14/symbols.js
+++ code/repositorio-ds/3p/ejemplos/gro-14/symbols.js
@@ -1,15 +1,20 @@
-let _items=Symbol();
 class Stack {
-constructor(){ 
-    this.items=[];}
-push(element){
- this.items.push(element);
+    constructor(){ 
+        this.items=[];}
+        push(element){
+            this.items.push(element);
+            return this.stack;
+           };
+           isEmpty(){
+            return this.items.length == 0;
+           };
+        pop(){
+            return this.items.pop();
+           };
+         print(){
+ 			console.log(this.items.toString());
 };
-
-print(){
- console.log(this.items.toString());
-};
-}
+    }
 let stack=new Stack();
 stack.push(5);
 stack.push(8);
